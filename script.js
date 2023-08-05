c4ab428866ef6605e00fad18213b03896acf86bd
Ardi Racaj <still_luffy@web.de>
2023-08-05 21:35:29 +0200

    implemented feature 5

--- script.js
+++ script.js
@@ -1,5 +1,34 @@
 const name = document.querySelector(".name");
 
-function changeColor() {
+function changeColor1() {
     name.style.color = "blue";
 }
+
+
+function changeColor2() {
+    name.style.color = "blue";
+}
+
+
+function changeColor3() {
+    name.style.color = "blue";
+}
+
+
+
+function changeColor4() {
+    name.style.color = "blue";
+}
+
+
+
+function changeColor5() {
+    name.style.color = "blue";
+}
+
+
+
+function changeColor6() {
+    name.style.color = "blue";
+}
+
